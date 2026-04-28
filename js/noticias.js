0c4fad6fdf15cca64675c3a12ab10b50b98715c1
VTomii <assesin25879@gmail.com>
2026-04-27 22:26:14 -0300

    arreglo los nombres de las clases en html y css (snake_case

--- js/noticias.js
+++ js/noticias.js
@@ -1,43 +1,45 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const contenedor = document.getElementById("Container_UltimasNoticias");
+    const contenedor = document.getElementById("container_ultimas_noticias");
 
-    let index = 0;
-    let slides = [];
+    let iteracion = 0;
+    const Noticias = [];
 
     // crear TODAS las cards una sola vez
     ultimasNoticias.forEach((Noticia, i) => {
         const card = document.createElement("div");
-        card.classList.add("CardUltimaNoticia");
+        card.classList.add("card_ultima_noticia");
 
         if (i === 0) card.classList.add("active");
 
         card.innerHTML = `
-            <img src="${Noticia.imagen}" class="card-img">
-            <div class="card-content">
-                <span>${Noticia.categoria}</span>
-                <h4>${Noticia.titulo}</h4>
-                <p>${Noticia.resumen}</p>
-                <small>${Noticia.autor}</small>
-            </div>
+            <img src="${Noticia.imagen}" class="card_img">
+                <div class="card_content">
+                    <span>${Noticia.categoria}</span>
+                    <h4>${Noticia.titulo}</h4>
+                    <p>${Noticia.resumen}</p>
+                    <small>${Noticia.autor}</small>
+                </div>
+            
         `;
 
         contenedor.appendChild(card);
-        slides.push(card);
+        Noticias.push(card);
+        console.log(card);
     });
-        function updateSlider() {
-        slides.forEach((slide, i) => {
-            slide.classList.toggle("active", i === index);
+        function updateNoticia() {
+        Noticias.forEach((Noticias, i) => {
+            Noticias.classList.toggle("active", i === iteracion);
         });
     }
     
     document.getElementById("next").addEventListener("click", () => {
-        index = (index + 1) % slides.length;
-        updateSlider();
+        iteracion = (iteracion + 1) %  Noticias.length;
+        updateNoticia();
     });
 
     document.getElementById("prev").addEventListener("click", () => {
-        index = (index - 1 + slides.length) % slides.length;
-        updateSlider();
+        iteracion = (iteracion - 1 + Noticias.length) % Noticias.length;
+        updateNoticia();
     });
 
 })
